48c24c7df8f6a06c780ff3e00b8042fc71061667
Zhai Can <bczhc0@126.com>
2025-09-22 21:46:28 +0800

    update full-color-chunks.txt

--- src/playground/find_full_color_chunks.rs
+++ src/playground/find_full_color_chunks.rs
@@ -1,3 +1,5 @@
+#![feature(file_buffered)]
+
 mod diff_size_of_chunks_changed;
 
 /// Find chunks that are fully painted (no transparency at all).
@@ -7,29 +9,40 @@ mod diff_size_of_chunks_changed;
 /// Wplace is expected not to put a zero in the `trns` field.
 use rayon::prelude::*;
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::path::Path;
+use std::sync::mpsc::channel;
 use wplace_tools::{collect_chunks, stylized_progress_bar};
 
+
 fn main() -> anyhow::Result<()> {
+    let mut output = File::create_buffered("output/full-color-chunks.txt")?;
+    let (tx, rx) = channel();
+
     let path = Path::new("/mnt/nvme/wplace-archives/1/2025-09-21T09-35-13.789Z+2h49m");
     let chunks = collect_chunks(path, None)?;
     let progress = stylized_progress_bar(chunks.len() as u64);
-    chunks.into_par_iter().for_each(|(x, y)| {
+    chunks.into_par_iter().for_each_with(tx, |sender, (x, y)| {
         let png_file = path.join(format!("{x}/{y}.png"));
 
         let png = png::Decoder::new(BufReader::new(File::open(&png_file).unwrap()));
         let reader = png.read_info().unwrap();
         let info = reader.info();
-        // I may expect if a chunk were painted fully (that's, no transparency pixels at all),
-        // the PNG encoder from Wplace may not put a `0` in the `trns` array.
+        // According to the spec, `trns` not having any zero means a full opaque PNG.
         let no_alpha = info.trns.as_ref().is_none_or(|x| !x.contains(&0));
         if no_alpha {
             println!("{:?}", (x, y));
+            sender.send(format!("{:?}", (x, y))).unwrap();
         }
 
         progress.inc(1);
     });
     progress.finish();
+
+    for x in rx {
+        use io::Write;
+        writeln!(&mut output, "{x}")?;
+    }
     Ok(())
 }
