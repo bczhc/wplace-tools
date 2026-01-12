bczhc/wplace-tools

commit 3c40e8377b1b06fb87836fe314e23828788d6545

archive-tool: diff: put chunk buffers on rayon `for_each_with`
## Benchmarks

```
Benchmark 1: ~/code/wplace-tools/target/release/archive-tool diff small_2026-01-11T22-49-33.937Z.tar small_2026-01-12T02-12-10.983Z.tar /tmp/a.diff
  Time (mean ± σ):      1.181 s ±  0.020 s    [User: 14.154 s, System: 3.108 s]
  Range (min … max):    1.154 s …  1.206 s    5 runs
```

vs

```
Benchmark 1: ~/code/wplace-tools/target/release/archive-tool diff small_2026-01-11T22-49-33.937Z.tar small_2026-01-12T02-12-10.983Z.tar /tmp/a.diff
  Time (mean ± σ):     959.8 ms ±   2.8 ms    [User: 13683.5 ms, System: 283.6 ms]
  Range (min … max):   956.9 ms … 963.6 ms    5 runs
```

src/bin/archive-tool.rs
@@ -7,7 +7,7 @@
 
 use crate::cli::Commands;
 use clap::Parser;
-use flate2::{Compression, write};
+use flate2::{write, Compression};
 use log::{debug, info};
 use rayon::prelude::*;
 use std::cell::RefCell;
@@ -22,9 +22,9 @@ use tempfile::NamedTempFile;
 use wplace_tools::checksum::chunk_checksum;
 use wplace_tools::indexed_png::{read_png, write_chunk_png};
 use wplace_tools::{
-    CHUNK_LENGTH, ChunkFetcher, ChunkProcessError, DirChunkFetcher, ExitOnError, MUTATION_MASK,
-    PALETTE_INDEX_MASK, TarChunkFetcher, apply_chunk, collect_chunks, diff3, new_chunk_file,
-    open_file_range, set_up_logger, stylized_progress_bar, validate_chunk_checksum,
+    apply_chunk, chunk_buf, collect_chunks, diff3, new_chunk_file, open_file_range,
+    set_up_logger, stylized_progress_bar, validate_chunk_checksum, ChunkFetcher, ChunkProcessError, DirChunkFetcher,
+    ExitOnError, TarChunkFetcher, CHUNK_LENGTH, MUTATION_MASK, PALETTE_INDEX_MASK,
 };
 use yeet_ops::yeet;
 
@@ -369,30 +369,30 @@ fn do_diff(
     let progress = stylized_progress_bar(new_fetcher.chunks_len() as u64);
     spawn(move || {
         let chunks_iter = new_fetcher.chunks_iter();
-        chunks_iter.par_bridge().for_each_with(tx, |tx, (x, y)| {
-            let result: anyhow::Result<()> = try {
-                let mut base_buf = vec![0_u8; CHUNK_LENGTH];
-                let mut new_buf = vec![0_u8; CHUNK_LENGTH];
+        chunks_iter.par_bridge().for_each_with(
+            (tx, chunk_buf!(), chunk_buf!()),
+            |(tx, base_buf, new_buf), (x, y)| {
+                let result: anyhow::Result<()> = try {
+                    let present = new_fetcher.fetch((x, y), new_buf)?;
+                    assert!(present);
+                    let base_chunk_present = base_fetcher.fetch((x, y), base_buf)?;
 
-                let present = new_fetcher.fetch((x, y), &mut new_buf)?;
-                assert!(present);
-                let base_chunk_present = base_fetcher.fetch((x, y), &mut base_buf)?;
+                    let checksum = chunk_checksum(&new_buf);
 
-                let checksum = chunk_checksum(&new_buf);
-
-                // It's expecting that a large percent of the chunks are not mutated.
-                // Thus in this case, only computing diff for changed chunks can reduce the process time.
-                let compressed_diff = if !base_chunk_present || base_buf != new_buf {
-                    let compressed_diff = diff_png_compressed(&mut base_buf, &new_buf).unwrap();
-                    Some(compressed_diff)
-                } else {
-                    None
+                    // It's expecting that a large percent of the chunks are not mutated.
+                    // Thus in this case, only computing diff for changed chunks can reduce the process time.
+                    let compressed_diff = if !base_chunk_present || base_buf != new_buf {
+                        let compressed_diff = diff_png_compressed(base_buf, &new_buf).unwrap();
+                        Some(compressed_diff)
+                    } else {
+                        None
+                    };
+                    tx.send((x, y, compressed_diff, checksum)).unwrap();
+                    progress.inc(1);
                 };
-                tx.send((x, y, compressed_diff, checksum)).unwrap();
-                progress.inc(1);
-            };
-            result.exit_on_error();
-        });
+                result.exit_on_error();
+            },
+        );
         progress.finish();
     });
 

src/lib.rs
@@ -634,3 +634,7 @@ impl DiffFilesCollector for SqfsDiffFilesCollector {
         Box::new(self.names.iter().map(|x| x.0))
     }
 }
+
+pub macro chunk_buf() {
+    vec![0_u8; CHUNK_LENGTH]
+}

commit f4b6234e7f3fa18996d08b6c5a19819b5744e3d1

fix bug introduced by 3c40e8377b1b06fb87836fe314e23828788d6545
The base_chunk should be all zeros on chunk image not exist.
3c40e8377b1b06fb87836fe314e23828788d6545 makes buffers reusable, so
the resetting operation should be explicitly done.

src/bin/archive-tool.rs
@@ -376,6 +376,9 @@ fn do_diff(
                     let present = new_fetcher.fetch((x, y), new_buf)?;
                     assert!(present);
                     let base_chunk_present = base_fetcher.fetch((x, y), base_buf)?;
+                    if !base_chunk_present {
+                        base_buf.fill(0);
+                    }
 
                     let checksum = chunk_checksum(&new_buf);
 